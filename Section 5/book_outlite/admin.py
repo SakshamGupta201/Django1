cf6ed298ebaa334635db422c14a896d5c7992f32
Saksham Gupta <saksham757gupta@gmail.com>
2023-12-25 12:03:18 +0530

    Section 7

--- Section 5/book_outlite/admin.py
+++ Section 5/book_outlite/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from book_outlite.models import Book
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("rating", "author")
+    list_display = ("title", "author")
+
+
+admin.site.register(Book, BookAdmin)
